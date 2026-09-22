1aa9e178e4a09b283f67f6e8808104004f0f257d
Tony Zaky <tonyzaky.s@gmail.com>
2026-09-21 22:54:42 -0500

    feat(fpga): connect key switches

--- hdl/ostdc_top.vhd
+++ hdl/ostdc_top.vhd
@@ -20,16 +20,21 @@ architecture rtl of ostdc_top is
     end component ostdc_sys;
 
     signal reset_n : std_logic;
+    signal internal_probe : std_logic_vector(9 downto 0);
 
 begin
 
     reset_n <= KEY(0);
 
+    -- Connect physical switches SW[7:0] to lower 8 bits, pad upper 2 bits with '0'
+    internal_probe(7 downto 0) <= SW(7 downto 0);
+    internal_probe(9 downto 8) <= (others => '0');
+
     u0 : component ostdc_sys
         port map (
             clk_clk                          => CLOCK_50,
             reset_reset_n                    => reset_n,
-            probe_inputs_export 					=> SW
+            probe_inputs_export              => internal_probe
         );
 
 end architecture rtl;
